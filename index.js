21b9e4be00723b6ca11fc5c1fabbc81eef79a0b9
Pedro Abreu <henriqueibtou@gmail.com>
2022-06-18 23:05:17 -0700

    Removed the coments on code, code looks more clean now everything working

--- index.js
+++ index.js
@@ -46,18 +46,7 @@ const onGetPictureFailure = () => {
   //  console.log('fail')
 }
 
-// document.addEventListener('DOMContentLoaded', () =>{
-//    // fetch(`https://www.reddit.com/r/pics/.json?q=${pictureNumber}+nsfw:no+type:image`)
 
-//     // .then(res  => res.json())
-//     // .then(onGetPictureSuccess)
-//     // .catch(onGetPictureFailure)
-
-// })
-
-
-//listen for a submit event and run said block of code
-//prevent to refresh the whole page
 form.addEventListener('submit', (event) =>{
     //prevent to refrresh the whole page
     event.preventDefault()
@@ -77,93 +66,3 @@ form.addEventListener('submit', (event) =>{
 
 
 
-// let numPictures = 15
-
-
-// const onShowPicturesSuccess = (fotos) =>{
-//     console.log(fotos)
-// }
-// const onShowPicturesFailure = () =>{
-//     console.log('failure')
-// }
-
-
-
-// const showPicture = (event) =>{
-//     const fotosURL = event.target.getAttribute('data-url')
-//     console.log(fotosURL)
-
-//     fetch(fotosURL)
-//     //turn you response obj in json
-//     .then(res => res.json())
-//     //in success
-//     .then(onShowPicturesSuccess)
-//     //in failure
-//     .catch(onShowPicturesFailure)
-
-// }
-
-
-
-// const newbox = document.createElement('div')
-    // newbox.classList.add('box-hold-picture')
-    // newbox.setAttribute('id', 'boxholdPicture')
-
-    // let images = pictureArray.data.children[numPictures].data.url
-
-    // if(images < images.length - 1){
-    //     images++;
-    // }else{
-    //     images = 0;
-    // }
-    // setTimeout("onGetPictureSuccess()" , 4000)
-    // form.addEventListener('submit', (event) =>{
-    //     //prevent to refrresh the whole page
-    //     event.preventDefault()
-    //     //const pictureNumber = input.value
-    //     //fetch(pictureArray.data.children[pictureNumber].data.url)
-    //     let numPictures = input.value
-    //     console.log('woot')
-    // })
-
-//call div  to click and show picture
-//const btnShowPicture = document.getElementById('btnShowPicture')
-//const boxholdPictures = document.getElementById('boxpicture')
-
-
-// function reloadPage(){
-//     ++numPictures
-//     const boxholdpic = document.getElementById('boxholdPicture')
-//     $("#boxholdPicture").load("#boxholdPicture");
-//    // $(ShowBoxPicture).load(window.location.href + ".box-hold-picture")
-//    // $('.box-hold-picture').load(self);
-//     //window.location.reload();
-//     //++numPictures
-//     console.log(numPictures)
-// }
-
-// Some sort of title
-// A short description telling the user what to do
-// A blank text field
-// A Button ("start" or "go" or "search")
-
-//let numPictures = $('#searchbar').val();
-
-
-
-//const seePictures = document.querySelector('#')
-
-// window.onbeforeunload = function(beforeWindow){
-//     localStorage.setItem("numPictures",$('#searchbar').val())
-// }
-
-// window.onload = function(afterWindow){
-//     var namep = localStorage.getItem("numPictures");
-//     if (namep !== null ) $('#searchbar').val("numPictures")
-// }
-
-// function reloadPage(event){
-//     window.location.reload();
-//     ++numPictures
-//     console.log(numPictures)
-// }
